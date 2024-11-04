91851e8140a2e2e0011b185869eb243ec3a962e7
elrincondehiro <administracion@elrincondehiro.com>
2024-11-04 14:05:43 +0100

    unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
-
 import vercel from '@astrojs/vercel/serverless';
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+  },
   output: 'server',
   integrations: [tailwind(), svelte()],
   adapter: vercel()
